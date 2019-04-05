9c6cd044f6be1cf112b22f47bc4a48b0846e62fb
M. Moutanin Brice <moutanin@insa-20157>
2019-04-05 13:56:31 +0200

    [PATH] getTravelTime() OK

--- src/main/org/insa/graph/Path.java
+++ src/main/org/insa/graph/Path.java
@@ -225,12 +225,13 @@ public class Path {
      * 
      * @return Time (in seconds) required to travel this path at the given speed (in
      *         kilometers-per-hour).
-     * 
-     * @deprecated Need to be implemented.
      */
     public double getTravelTime(double speed) {
-        // TODO:
-        return 0;
+        double time = 0;
+        for (int i=0;i<this.arcs.size();i++) {
+        	time += this.arcs.get(i).getTravelTime(speed);
+        }
+        return time;
     }
 
     /**
