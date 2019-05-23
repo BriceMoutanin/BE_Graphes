4d70310791dc4203fe1e7af633eaa12f00adabcf
Baptiste Mennesson <babar@Ubuntu.Baptiste>
2019-05-23 19:54:11 +0200

    [ValidityTests] Début implémentation sans oracle

--- src/test/org/insa/algo/shortestpath/AStarValidityTest.java
+++ src/test/org/insa/algo/shortestpath/AStarValidityTest.java
@@ -23,10 +23,16 @@ public class AStarValidityTest {
 	private static ShortestPathData data_gr1_faisable_l, data_gr1_non_connexe_l, data_gr1_identique_l;
 	private static ShortestPathData data_gr2_faisable_l, data_gr2_non_connexe_l, data_gr2_identique_l;
 	private static ShortestPathData data_gr3_faisable_l, data_gr3_identique_l;
+	private static ShortestPathData data_gr1_faisablev2_l;
+	private static ShortestPathData data_gr2_faisablev2_l;
+	private static ShortestPathData data_gr3_faisablev2_l;
 	
 	private static AStarAlgorithm solD_data_gr1_faisable_l, solD_data_gr1_non_connexe_l, solD_data_gr1_identique_l;
 	private static AStarAlgorithm solD_data_gr2_faisable_l, solD_data_gr2_non_connexe_l, solD_data_gr2_identique_l;
 	private static AStarAlgorithm solD_data_gr3_faisable_l, solD_data_gr3_identique_l;
+	private static AStarAlgorithm solD_data_gr1_faisablev2_l;
+	private static AStarAlgorithm solD_data_gr2_faisablev2_l;
+	private static AStarAlgorithm solD_data_gr3_faisablev2_l;
 	
 	private static DijkstraAlgorithm solB_data_gr1_faisable_l;
 	private static DijkstraAlgorithm solB_data_gr2_faisable_l;
@@ -36,6 +42,10 @@ public class AStarValidityTest {
 	private static Path pathD_data_gr2_faisable_l;
 	private static Path pathD_data_gr3_faisable_l;
 	
+	private static Path pathD_data_gr1_faisablev2_l;
+	private static Path pathD_data_gr2_faisablev2_l;
+	private static Path pathD_data_gr3_faisablev2_l;
+	
 	private static Path pathB_data_gr1_faisable_l;
 	private static Path pathB_data_gr2_faisable_l;
 	private static Path pathB_data_gr3_faisable_l;
@@ -44,10 +54,16 @@ public class AStarValidityTest {
 	private static ShortestPathData data_gr1_faisable_t, data_gr1_non_connexe_t, data_gr1_identique_t;
 	private static ShortestPathData data_gr2_faisable_t, data_gr2_non_connexe_t, data_gr2_identique_t;
 	private static ShortestPathData data_gr3_faisable_t, data_gr3_identique_t;
+	private static ShortestPathData data_gr1_faisablev2_t;
+	private static ShortestPathData data_gr2_faisablev2_t;
+	private static ShortestPathData data_gr3_faisablev2_t;
 		
 	private static AStarAlgorithm solD_data_gr1_faisable_t, solD_data_gr1_non_connexe_t, solD_data_gr1_identique_t;
 	private static AStarAlgorithm solD_data_gr2_faisable_t, solD_data_gr2_non_connexe_t, solD_data_gr2_identique_t;
 	private static AStarAlgorithm solD_data_gr3_faisable_t, solD_data_gr3_identique_t;
+	private static AStarAlgorithm solD_data_gr1_faisablev2_t;
+	private static AStarAlgorithm solD_data_gr2_faisablev2_t;
+	private static AStarAlgorithm solD_data_gr3_faisablev2_t;
 		
 	private static DijkstraAlgorithm solB_data_gr1_faisable_t;
 	private static DijkstraAlgorithm solB_data_gr2_faisable_t;
@@ -56,6 +72,10 @@ public class AStarValidityTest {
 	private static Path pathD_data_gr1_faisable_t;
 	private static Path pathD_data_gr2_faisable_t;
 	private static Path pathD_data_gr3_faisable_t;
+	
+	private static Path pathD_data_gr1_faisablev2_t;
+	private static Path pathD_data_gr2_faisablev2_t;
+	private static Path pathD_data_gr3_faisablev2_t;
 		
 	private static Path pathB_data_gr1_faisable_t;
 	private static Path pathB_data_gr2_faisable_t;
@@ -76,9 +96,14 @@ public class AStarValidityTest {
     //String mapName3 = "/home/commetud/3eme Annee MIC/Graphes-et-Algorithmes/Maps/fractal-spiral.mapgr";
     
     // The following lines are just supposed to allow us to run our tests on our own computers //
-    String mapName1 = "/Users/bricemoutanin/Downloads/new-zealand.mapgr";
-    String mapName2 = "/Users/bricemoutanin/Downloads//paris.mapgr";
-    String mapName3 = "/Users/bricemoutanin/Downloads/fractal-spiral.mapgr";
+    // String mapName1 = "/Users/bricemoutanin/Downloads/new-zealand.mapgr";
+    // String mapName2 = "/Users/bricemoutanin/Downloads//paris.mapgr";
+    // String mapName3 = "/Users/bricemoutanin/Downloads/fractal-spiral.mapgr";
+	
+    String mapName1 = "/home/babar/Bureau/maps/new-zealand.mapgr";
+    String mapName2 = "/home/babar/Bureau/maps/paris.mapgr";
+    String mapName3 = "/home/babar/Bureau/maps/fractal-spiral.mapgr";
+	
     /////////////////////////////////////////////////////////////////////////////////////////////
 
     // Create a graph reader.
@@ -115,6 +140,12 @@ public class AStarValidityTest {
     data_gr3_faisable_l = new ShortestPathData(graph3, graph3.getNodes().get(822642), graph3.getNodes().get(323463) , arc_insp_length);
     data_gr3_identique_l = new ShortestPathData(graph3, graph3.getNodes().get(323463), graph3.getNodes().get(323463) , arc_insp_length);
     
+    /** Data construction with reversed destination and origin */
+    
+    data_gr1_faisablev2_l = new ShortestPathData(graph1, graph1.getNodes().get(243205), graph1.getNodes().get(327010) , arc_insp_length);
+    data_gr2_faisablev2_l = new ShortestPathData(graph2, graph2.getNodes().get(38451), graph2.getNodes().get(5287) , arc_insp_length);
+    data_gr3_faisablev2_l = new ShortestPathData(graph3, graph3.getNodes().get(323463), graph3.getNodes().get(822642) , arc_insp_length);
+    
     /** AStar solutions definitions */
     
     solD_data_gr1_faisable_l = new AStarAlgorithm(data_gr1_faisable_l) ;
@@ -128,12 +159,20 @@ public class AStarValidityTest {
     solD_data_gr3_faisable_l = new AStarAlgorithm(data_gr3_faisable_l) ;
     solD_data_gr3_identique_l = new AStarAlgorithm(data_gr3_identique_l) ;
     
+    /* AStar reversed solutions definitions */
+    
+    solD_data_gr1_faisablev2_l = new AStarAlgorithm(data_gr1_faisablev2_l) ;
+    solD_data_gr2_faisablev2_l = new AStarAlgorithm(data_gr2_faisablev2_l) ;
+    solD_data_gr3_faisablev2_l = new AStarAlgorithm(data_gr3_faisablev2_l) ;
+    
     
     /** Dijkstra solutions definitions */
 
     solB_data_gr1_faisable_l = new DijkstraAlgorithm(data_gr1_faisable_l) ;
     solB_data_gr2_faisable_l = new DijkstraAlgorithm(data_gr2_faisable_l) ;
     solB_data_gr3_faisable_l = new DijkstraAlgorithm(data_gr3_faisable_l) ;
+    
+    /** Path definitions */
     
     pathD_data_gr1_faisable_l = solD_data_gr1_faisable_l.doRun().getPath();
 	pathD_data_gr2_faisable_l = solD_data_gr2_faisable_l.doRun().getPath();
@@ -142,6 +181,14 @@ public class AStarValidityTest {
 	pathB_data_gr1_faisable_l = solB_data_gr1_faisable_l.doRun().getPath();
 	pathB_data_gr2_faisable_l = solB_data_gr2_faisable_l.doRun().getPath();
 	pathB_data_gr3_faisable_l = solB_data_gr3_faisable_l.doRun().getPath();
+	
+	/** Reversed Path definitions */
+	
+    pathD_data_gr1_faisablev2_l = solD_data_gr1_faisablev2_l.doRun().getPath();
+	pathD_data_gr2_faisablev2_l = solD_data_gr2_faisablev2_l.doRun().getPath();
+	pathD_data_gr3_faisablev2_l = solD_data_gr3_faisablev2_l.doRun().getPath();
+	
+	
 
 	/** Data constructions for the time tests *////////////////////////////////////////////////////////////////////////////////////////
     
@@ -156,6 +203,13 @@ public class AStarValidityTest {
     data_gr3_faisable_t = new ShortestPathData(graph3, graph3.getNodes().get(822642), graph3.getNodes().get(323463) , arc_insp_time);
     data_gr3_identique_t = new ShortestPathData(graph3, graph3.getNodes().get(323463), graph3.getNodes().get(323463) , arc_insp_time);
     
+    
+    /** Data construction with reversed destination and origin */
+    
+    data_gr1_faisablev2_t = new ShortestPathData(graph1, graph1.getNodes().get(243205), graph1.getNodes().get(327010) , arc_insp_time);
+    data_gr2_faisablev2_t = new ShortestPathData(graph2, graph2.getNodes().get(38451), graph2.getNodes().get(5287) , arc_insp_time);
+    data_gr3_faisablev2_t = new ShortestPathData(graph3, graph3.getNodes().get(323463), graph3.getNodes().get(822642) , arc_insp_time);
+    
     /** AStar solutions definitions */
     
     solD_data_gr1_faisable_t = new AStarAlgorithm(data_gr1_faisable_t) ;
@@ -169,12 +223,20 @@ public class AStarValidityTest {
     solD_data_gr3_faisable_t = new AStarAlgorithm(data_gr3_faisable_t) ;
     solD_data_gr3_identique_t = new AStarAlgorithm(data_gr3_identique_t) ;
     
+    /* AStar reversed solutions definitions */
+    
+    solD_data_gr1_faisablev2_t = new AStarAlgorithm(data_gr1_faisablev2_t) ;
+    solD_data_gr2_faisablev2_t = new AStarAlgorithm(data_gr2_faisablev2_t) ;
+    solD_data_gr3_faisablev2_t = new AStarAlgorithm(data_gr3_faisablev2_t) ;
+    
     
     /** Dijkstra solutions definitions */
 
     solB_data_gr1_faisable_t = new DijkstraAlgorithm(data_gr1_faisable_t) ;
     solB_data_gr2_faisable_t = new DijkstraAlgorithm(data_gr2_faisable_t) ;
     solB_data_gr3_faisable_t = new DijkstraAlgorithm(data_gr3_faisable_t) ;
+    
+    /** Path definitions */
     
     pathD_data_gr1_faisable_t = solD_data_gr1_faisable_t.doRun().getPath();
 	pathD_data_gr2_faisable_t = solD_data_gr2_faisable_t.doRun().getPath();
@@ -183,14 +245,20 @@ public class AStarValidityTest {
 	pathB_data_gr1_faisable_t = solB_data_gr1_faisable_t.doRun().getPath();
 	pathB_data_gr2_faisable_t = solB_data_gr2_faisable_t.doRun().getPath();
 	pathB_data_gr3_faisable_t = solB_data_gr3_faisable_t.doRun().getPath();
+	
+	/** Reversed Path definitions */
+	
+    pathD_data_gr1_faisablev2_t = solD_data_gr1_faisablev2_t.doRun().getPath();
+	pathD_data_gr2_faisablev2_t = solD_data_gr2_faisablev2_t.doRun().getPath();
+	pathD_data_gr3_faisablev2_t = solD_data_gr3_faisablev2_t.doRun().getPath();
     
 	}
     
     @Test
     public void testValid_LengthMode() {
-    	assertTrue(solD_data_gr1_faisable_l.doRun().getPath().isValid());
-    	assertTrue(solD_data_gr2_faisable_l.doRun().getPath().isValid());
-    	assertTrue(solD_data_gr3_faisable_l.doRun().getPath().isValid());
+    	assertTrue(pathD_data_gr1_faisable_l.isValid());
+    	assertTrue(pathD_data_gr2_faisable_l.isValid());
+    	assertTrue(pathD_data_gr3_faisable_l.isValid());
     	assertEquals(solD_data_gr1_non_connexe_l.doRun().getPath(),null);
     	assertEquals(solD_data_gr2_non_connexe_l.doRun().getPath(),null);
     	assertEquals(solD_data_gr1_identique_l.doRun().getPath(),null);
@@ -200,9 +268,9 @@ public class AStarValidityTest {
     
     @Test
     public void testValid_TimeMode() {
-    	assertTrue(solD_data_gr1_faisable_t.doRun().getPath().isValid());
-    	assertTrue(solD_data_gr2_faisable_t.doRun().getPath().isValid());
-    	assertTrue(solD_data_gr3_faisable_t.doRun().getPath().isValid());
+    	assertTrue(pathD_data_gr1_faisable_t.isValid());
+    	assertTrue(pathD_data_gr2_faisable_t.isValid());
+    	assertTrue(pathD_data_gr3_faisable_t.isValid());
     	assertEquals(solD_data_gr1_non_connexe_t.doRun().getPath(),null);
     	assertEquals(solD_data_gr2_non_connexe_t.doRun().getPath(),null);
     	assertEquals(solD_data_gr1_identique_t.doRun().getPath(),null);
@@ -236,6 +304,20 @@ public class AStarValidityTest {
         assertEquals(pathD_data_gr1_faisable_t.getLength(), pathB_data_gr1_faisable_t.getLength(),1e-6);
         assertEquals(pathD_data_gr2_faisable_t.getLength(), pathB_data_gr2_faisable_t.getLength(),1e-6);
         assertEquals(pathD_data_gr3_faisable_t.getLength(), pathB_data_gr3_faisable_t.getLength(),1e-6);    	
+    }
+    
+    @Test
+    public void NoOracle_reversedPathEquality_LengthMode() {
+        assertEquals(pathD_data_gr1_faisable_l.getLength(), pathD_data_gr1_faisablev2_l.getLength(),1e3);
+        assertEquals(pathD_data_gr2_faisable_l.getLength(), pathD_data_gr2_faisablev2_l.getLength(),1e3);
+        assertEquals(pathD_data_gr3_faisable_l.getLength(), pathD_data_gr3_faisablev2_l.getLength(),1e3);    	
+    }
+    
+    @Test
+    public void NoOracle_reversedPathEquality_TimeMode() {
+        assertEquals(pathD_data_gr1_faisable_t.getMinimumTravelTime(), pathD_data_gr1_faisablev2_t.getMinimumTravelTime(),2e2);
+        assertEquals(pathD_data_gr2_faisable_t.getMinimumTravelTime(), pathD_data_gr2_faisablev2_t.getMinimumTravelTime(),2e2);
+        assertEquals(pathD_data_gr3_faisable_t.getMinimumTravelTime(), pathD_data_gr3_faisablev2_t.getMinimumTravelTime(),2e2);    	
     }
     
     //Vitesse differentes
--- src/test/org/insa/algo/shortestpath/DijkstraValidityTest.java
+++ src/test/org/insa/algo/shortestpath/DijkstraValidityTest.java
@@ -23,10 +23,16 @@ public class DijkstraValidityTest {
 	private static ShortestPathData data_gr1_faisable_l, data_gr1_non_connexe_l, data_gr1_identique_l;
 	private static ShortestPathData data_gr2_faisable_l, data_gr2_non_connexe_l, data_gr2_identique_l;
 	private static ShortestPathData data_gr3_faisable_l, data_gr3_identique_l;
+	private static ShortestPathData data_gr1_faisablev2_l;
+	private static ShortestPathData data_gr2_faisablev2_l;
+	private static ShortestPathData data_gr3_faisablev2_l;
 	
 	private static DijkstraAlgorithm solD_data_gr1_faisable_l, solD_data_gr1_non_connexe_l, solD_data_gr1_identique_l;
 	private static DijkstraAlgorithm solD_data_gr2_faisable_l, solD_data_gr2_non_connexe_l, solD_data_gr2_identique_l;
 	private static DijkstraAlgorithm solD_data_gr3_faisable_l, solD_data_gr3_identique_l;
+	private static DijkstraAlgorithm solD_data_gr1_faisablev2_l;
+	private static DijkstraAlgorithm solD_data_gr2_faisablev2_l;
+	private static DijkstraAlgorithm solD_data_gr3_faisablev2_l;
 	
 	private static BellmanFordAlgorithm solB_data_gr1_faisable_l;
 	private static BellmanFordAlgorithm solB_data_gr2_faisable_l;
@@ -36,6 +42,10 @@ public class DijkstraValidityTest {
 	private static Path pathD_data_gr2_faisable_l;
 	private static Path pathD_data_gr3_faisable_l;
 	
+	private static Path pathD_data_gr1_faisablev2_l;
+	private static Path pathD_data_gr2_faisablev2_l;
+	private static Path pathD_data_gr3_faisablev2_l;
+	
 	private static Path pathB_data_gr1_faisable_l;
 	private static Path pathB_data_gr2_faisable_l;
 	private static Path pathB_data_gr3_faisable_l;
@@ -44,14 +54,24 @@ public class DijkstraValidityTest {
 	private static ShortestPathData data_gr1_faisable_t, data_gr1_non_connexe_t, data_gr1_identique_t;
 	private static ShortestPathData data_gr2_faisable_t, data_gr2_non_connexe_t, data_gr2_identique_t;
 	private static ShortestPathData data_gr3_faisable_t, data_gr3_identique_t;
+	private static ShortestPathData data_gr1_faisablev2_t;
+	private static ShortestPathData data_gr2_faisablev2_t;
+	private static ShortestPathData data_gr3_faisablev2_t;
 		
 	private static DijkstraAlgorithm solD_data_gr1_faisable_t, solD_data_gr1_non_connexe_t, solD_data_gr1_identique_t;
 	private static DijkstraAlgorithm solD_data_gr2_faisable_t, solD_data_gr2_non_connexe_t, solD_data_gr2_identique_t;
 	private static DijkstraAlgorithm solD_data_gr3_faisable_t, solD_data_gr3_identique_t;
+	private static DijkstraAlgorithm solD_data_gr1_faisablev2_t;
+	private static DijkstraAlgorithm solD_data_gr2_faisablev2_t;
+	private static DijkstraAlgorithm solD_data_gr3_faisablev2_t;
 		
 	private static BellmanFordAlgorithm solB_data_gr1_faisable_t;
 	private static BellmanFordAlgorithm solB_data_gr2_faisable_t;
 	private static BellmanFordAlgorithm solB_data_gr3_faisable_t;
+	
+	private static Path pathD_data_gr1_faisablev2_t;
+	private static Path pathD_data_gr2_faisablev2_t;
+	private static Path pathD_data_gr3_faisablev2_t;
 		
 	private static Path pathD_data_gr1_faisable_t;
 	private static Path pathD_data_gr2_faisable_t;
@@ -76,9 +96,14 @@ public class DijkstraValidityTest {
     //String mapName3 = "/home/commetud/3eme Annee MIC/Graphes-et-Algorithmes/Maps/fractal-spiral.mapgr";
     
     // The following lines are just supposed to allow us to run our tests on our own computers //
-    String mapName1 = "/Users/bricemoutanin/Downloads/new-zealand.mapgr";
-    String mapName2 = "/Users/bricemoutanin/Downloads//paris.mapgr";
-    String mapName3 = "/Users/bricemoutanin/Downloads/fractal-spiral.mapgr";
+    // String mapName1 = "/Users/bricemoutanin/Downloads/new-zealand.mapgr";
+    // String mapName2 = "/Users/bricemoutanin/Downloads//paris.mapgr";
+    // String mapName3 = "/Users/bricemoutanin/Downloads/fractal-spiral.mapgr";
+	
+    String mapName1 = "/home/babar/Bureau/maps/new-zealand.mapgr";
+    String mapName2 = "/home/babar/Bureau/maps/paris.mapgr";
+    String mapName3 = "/home/babar/Bureau/maps/fractal-spiral.mapgr";
+	
     /////////////////////////////////////////////////////////////////////////////////////////////
 
     // Create a graph reader.
@@ -115,6 +140,12 @@ public class DijkstraValidityTest {
     data_gr3_faisable_l = new ShortestPathData(graph3, graph3.getNodes().get(822642), graph3.getNodes().get(323463) , arc_insp_length);
     data_gr3_identique_l = new ShortestPathData(graph3, graph3.getNodes().get(323463), graph3.getNodes().get(323463) , arc_insp_length);
     
+    /** Data construction with reversed destination and origin */
+    
+    data_gr1_faisablev2_l = new ShortestPathData(graph1, graph1.getNodes().get(243205), graph1.getNodes().get(327010) , arc_insp_length);
+    data_gr2_faisablev2_l = new ShortestPathData(graph2, graph2.getNodes().get(38451), graph2.getNodes().get(5287) , arc_insp_length);
+    data_gr3_faisablev2_l = new ShortestPathData(graph3, graph3.getNodes().get(323463), graph3.getNodes().get(822642) , arc_insp_length);
+    
     /** Dijkstra solutions definitions */
     
     solD_data_gr1_faisable_l = new DijkstraAlgorithm(data_gr1_faisable_l) ;
@@ -128,12 +159,21 @@ public class DijkstraValidityTest {
     solD_data_gr3_faisable_l = new DijkstraAlgorithm(data_gr3_faisable_l) ;
     solD_data_gr3_identique_l = new DijkstraAlgorithm(data_gr3_identique_l) ;
     
+    /* Dijkstra reversed solutions definitions */
+    
+    solD_data_gr1_faisablev2_l = new DijkstraAlgorithm(data_gr1_faisablev2_l) ;
+    solD_data_gr2_faisablev2_l = new DijkstraAlgorithm(data_gr2_faisablev2_l) ;
+    solD_data_gr3_faisablev2_l = new DijkstraAlgorithm(data_gr3_faisablev2_l) ;
+    
     
     /** Bellman-Ford solutions definitions */
 
     solB_data_gr1_faisable_l = new BellmanFordAlgorithm(data_gr1_faisable_l) ;
     solB_data_gr2_faisable_l = new BellmanFordAlgorithm(data_gr2_faisable_l) ;
     solB_data_gr3_faisable_l = new BellmanFordAlgorithm(data_gr3_faisable_l) ;
+    
+    
+    /** Path definitions */
     
     pathD_data_gr1_faisable_l = solD_data_gr1_faisable_l.doRun().getPath();
 	pathD_data_gr2_faisable_l = solD_data_gr2_faisable_l.doRun().getPath();
@@ -142,6 +182,14 @@ public class DijkstraValidityTest {
 	pathB_data_gr1_faisable_l = solB_data_gr1_faisable_l.doRun().getPath();
 	pathB_data_gr2_faisable_l = solB_data_gr2_faisable_l.doRun().getPath();
 	pathB_data_gr3_faisable_l = solB_data_gr3_faisable_l.doRun().getPath();
+	
+	/** Reversed Path definitions */
+	
+    pathD_data_gr1_faisablev2_l = solD_data_gr1_faisablev2_l.doRun().getPath();
+	pathD_data_gr2_faisablev2_l = solD_data_gr2_faisablev2_l.doRun().getPath();
+	pathD_data_gr3_faisablev2_l = solD_data_gr3_faisablev2_l.doRun().getPath();
+	
+	
 
 	/** Data constructions for the time tests *////////////////////////////////////////////////////////////////////////////////////////
     
@@ -156,6 +204,12 @@ public class DijkstraValidityTest {
     data_gr3_faisable_t = new ShortestPathData(graph3, graph3.getNodes().get(822642), graph3.getNodes().get(323463) , arc_insp_time);
     data_gr3_identique_t = new ShortestPathData(graph3, graph3.getNodes().get(323463), graph3.getNodes().get(323463) , arc_insp_time);
     
+    /** Data construction with reversed destination and origin */
+    
+    data_gr1_faisablev2_t = new ShortestPathData(graph1, graph1.getNodes().get(243205), graph1.getNodes().get(327010) , arc_insp_time);
+    data_gr2_faisablev2_t = new ShortestPathData(graph2, graph2.getNodes().get(38451), graph2.getNodes().get(5287) , arc_insp_time);
+    data_gr3_faisablev2_t = new ShortestPathData(graph3, graph3.getNodes().get(323463), graph3.getNodes().get(822642) , arc_insp_time);
+    
     /** Dijkstra solutions definitions */
     
     solD_data_gr1_faisable_t = new DijkstraAlgorithm(data_gr1_faisable_t) ;
@@ -169,12 +223,20 @@ public class DijkstraValidityTest {
     solD_data_gr3_faisable_t = new DijkstraAlgorithm(data_gr3_faisable_t) ;
     solD_data_gr3_identique_t = new DijkstraAlgorithm(data_gr3_identique_t) ;
     
+    /* Dijkstra reversed solutions definitions */
+    
+    solD_data_gr1_faisablev2_t = new DijkstraAlgorithm(data_gr1_faisablev2_t) ;
+    solD_data_gr2_faisablev2_t = new DijkstraAlgorithm(data_gr2_faisablev2_t) ;
+    solD_data_gr3_faisablev2_t = new DijkstraAlgorithm(data_gr3_faisablev2_t) ;
+    
     
     /** Bellman-Ford solutions definitions */
 
     solB_data_gr1_faisable_t = new BellmanFordAlgorithm(data_gr1_faisable_t) ;
     solB_data_gr2_faisable_t = new BellmanFordAlgorithm(data_gr2_faisable_t) ;
     solB_data_gr3_faisable_t = new BellmanFordAlgorithm(data_gr3_faisable_t) ;
+    
+    /** Path definitions */
     
     pathD_data_gr1_faisable_t = solD_data_gr1_faisable_t.doRun().getPath();
 	pathD_data_gr2_faisable_t = solD_data_gr2_faisable_t.doRun().getPath();
@@ -183,14 +245,20 @@ public class DijkstraValidityTest {
 	pathB_data_gr1_faisable_t = solB_data_gr1_faisable_t.doRun().getPath();
 	pathB_data_gr2_faisable_t = solB_data_gr2_faisable_t.doRun().getPath();
 	pathB_data_gr3_faisable_t = solB_data_gr3_faisable_t.doRun().getPath();
+	
+	/** Reversed Path definitions */
+	
+    pathD_data_gr1_faisablev2_t = solD_data_gr1_faisablev2_t.doRun().getPath();
+	pathD_data_gr2_faisablev2_t = solD_data_gr2_faisablev2_t.doRun().getPath();
+	pathD_data_gr3_faisablev2_t = solD_data_gr3_faisablev2_t.doRun().getPath();
     
 	}
     
     @Test
     public void testValid_LengthMode() {
-    	assertTrue(solD_data_gr1_faisable_l.doRun().getPath().isValid());
-    	assertTrue(solD_data_gr2_faisable_l.doRun().getPath().isValid());
-    	assertTrue(solD_data_gr3_faisable_l.doRun().getPath().isValid());
+    	assertTrue(pathD_data_gr1_faisable_l.isValid());
+    	assertTrue(pathD_data_gr2_faisable_l.isValid());
+    	assertTrue(pathD_data_gr3_faisable_l.isValid());
     	assertEquals(solD_data_gr1_non_connexe_l.doRun().getPath(),null);
     	assertEquals(solD_data_gr2_non_connexe_l.doRun().getPath(),null);
     	assertEquals(solD_data_gr1_identique_l.doRun().getPath(),null);
@@ -200,9 +268,9 @@ public class DijkstraValidityTest {
     
     @Test
     public void testValid_TimeMode() {
-    	assertTrue(solD_data_gr1_faisable_t.doRun().getPath().isValid());
-    	assertTrue(solD_data_gr2_faisable_t.doRun().getPath().isValid());
-    	assertTrue(solD_data_gr3_faisable_t.doRun().getPath().isValid());
+    	assertTrue(pathD_data_gr1_faisable_t.isValid());
+    	assertTrue(pathD_data_gr2_faisable_t.isValid());
+    	assertTrue(pathD_data_gr3_faisable_t.isValid());
     	assertEquals(solD_data_gr1_non_connexe_t.doRun().getPath(),null);
     	assertEquals(solD_data_gr2_non_connexe_t.doRun().getPath(),null);
     	assertEquals(solD_data_gr1_identique_t.doRun().getPath(),null);
@@ -238,8 +306,23 @@ public class DijkstraValidityTest {
         assertEquals(pathD_data_gr3_faisable_t.getLength(), pathB_data_gr3_faisable_t.getLength(),1e-6);    	
     }
     
+    
+    @Test
+    public void NoOracle_reversedPathEquality_LengthMode() {
+        assertEquals(pathD_data_gr1_faisable_l.getLength(), pathD_data_gr1_faisablev2_l.getLength(),1e3);
+        assertEquals(pathD_data_gr2_faisable_l.getLength(), pathD_data_gr2_faisablev2_l.getLength(),1e3);
+        assertEquals(pathD_data_gr3_faisable_l.getLength(), pathD_data_gr3_faisablev2_l.getLength(),1e3);    	
+    }
+    
+    @Test
+    public void NoOracle_reversedPathEquality_TimeMode() {
+        assertEquals(pathD_data_gr1_faisable_t.getMinimumTravelTime(), pathD_data_gr1_faisablev2_t.getMinimumTravelTime(),2e2);
+        assertEquals(pathD_data_gr2_faisable_t.getMinimumTravelTime(), pathD_data_gr2_faisablev2_t.getMinimumTravelTime(),2e2);
+        assertEquals(pathD_data_gr3_faisable_t.getMinimumTravelTime(), pathD_data_gr3_faisablev2_t.getMinimumTravelTime(),2e2);    	
+    }
+    
+    
     //Vitesse differentes
     //sous chemin
-    // inverser origine et destination
     
 }
